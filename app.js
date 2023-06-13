452b391d17893dd9a6a5e126ea4ca6e0df277ff4
Umer Khokhar <creativeinovators@gmail.com>
2023-06-13 10:25:44 +0500

    Rebuild app.js

--- app.js
+++ app.js
@@ -1,47 +1,25 @@
-// app.js
-
-// Select the parent element and the output element
-// const calculator = document.querySelector('.calculator');
-// const output = document.querySelector('.output');
-
-// // Add event listener to the parent element
-// calculator.addEventListener('click', (event) => {
-//   const target = event.target;
-//   const value = target.value;
-
-//   if (target.tagName === 'INPUT') {
-//     if (value === '=') {
-//       output.value = eval(output.value);
-//     } else if (value === 'AC') {
-//       output.value = '';
-//     } else if (value === 'DE') {
-//       output.value = output.value.slice(0, -1);
-//     } else {
-//       output.value += value;
-//     }
-//   }
-// });
 const calculator = document.querySelector('.calculator')
 const output = document.querySelector('.output')
-let divide = document.querySelector('divide')
 
-calculator.addEventListener('click', e => {
-  let target = e.target;
-  let value = target.value;
-  if (target.tagName === "INPUT") {
-    if (value === "=") {
-      output.value = eval(output.value)
-    } else if (value === "AC") {
-      output.value = ""
-    } else if (value === "DE") {
-      output.value = output.value.slice(0, -1)
-    } else if (value === "÷") {
-      output.value += "/";
-    } else {
-      output.value += value
+calculator.addEventListener('click', (e) => {
+    let allTarget = e.target
+    let value = allTarget.value
+
+    if (allTarget.tagName === "INPUT") {
+      if (value === "AC") {
+        output.value = ""
+      } else if (value === "DE") {
+        output.value = output.value.slice(0, -1)
+      } else if (value === "=") {
+        output.value = eval(output.value)
+      } else if (value === "÷") {
+        output.value += '/'
+      } else {
+        output.value += value
+      }
     }
-  }
 })
+
 window.addEventListener('load', e => {
-  output.value = ''
+  output.value = ""
 })
